a426c5d9ccafc2ba5f54360449a3282879205a5b
mattyrichards <matt@kreative.co.uk>
2016-03-25 14:28:00 +0000

    Home component refactored as HMR doesn't currently support re-rendering stateless functions. Which would kinda make HMR redundant on this boilerplate.

--- src/js/components/Home.js
+++ src/js/components/Home.js
@@ -3,16 +3,24 @@ import { Link } from 'react-router';
 
 import '../../scss/Home.scss';
 
-// "Traditional" style stateless function
-function Home() {
-  const pageNumber = 666;
-  return (
-    <div>
-      <p>This page has been rendered by React.</p>
-      <p><Link to={`/page/${pageNumber}`}>An example page</Link>
-      , demonstrates react-router.</p>
-    </div>
-  );
+class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      demoState: 'React!!!',
+    };
+  }
+
+  render() {
+    const pageNumber = 666;
+    return (
+      <div>
+        <p>This page has been rendered by {this.state.demoState}</p>
+        <p><Link to={`/page/${pageNumber}`}>An example page</Link>
+        , demonstrates react-router.</p>
+      </div>
+    );
+  }
 }
 
 export default Home;
