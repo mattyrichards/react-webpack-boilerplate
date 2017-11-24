e96174381a5b691eba1f56869e5054bf191a368a
Matt Richards <matt@kreative.co.uk>
2017-11-24 12:58:59 +0000

    Adds Redux dev tools.

--- src/js/entry.js
+++ src/js/entry.js
@@ -4,20 +4,27 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import promise from "redux-promise";
-import reducers from "./reducers";
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 
-/*
-	Import Components
-*/
+// Import reducers
+import rootReducer from "./reducers";
+
+// Import components/containers
 import NotFound from './components/NotFound';
 import Home from './components/Home';
 import Page from './components/Page';
 import Redux from './containers/Redux';
 
-const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+// Create Redux store, apply middlewares etc
+const store = createStore(
+	rootReducer,
+	composeWithDevTools(
+		applyMiddleware(promise)
+	)
+);
 
 ReactDOM.render((
-	<Provider store={createStoreWithMiddleware(reducers)}>
+	<Provider store={store}>
 		<Router>
 			<Switch>
 				<Route path="/page/:pageNumber" component={Page} />
