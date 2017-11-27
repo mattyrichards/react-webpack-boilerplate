8ace2dc9a981a3315037e784a997a68fc243c9b5
Matt Richards <matt@kreative.co.uk>
2017-11-27 23:07:59 +0000

    Adds comments to webpack dev config.

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,14 +4,21 @@ const config = require('./config');
 
 module.exports = {
   entry: [
+    // Required for HOT reloading in React
     'react-hot-loader/patch',
+    // Configure dev server address/port
     'webpack-dev-server/client?http://localhost:8080',
+    // only- means to only hot reload for successful updates
     'webpack/hot/only-dev-server',
+    // Entry point of application
     config.pathSource + config.pathJSAbsolute + config.filenameEntry
   ],
   output: {
+    // Filename requires relative path to JS folder
     filename: config.pathJSRelative + config.filenameOutput,
+    // Path used for all output assets
     path: config.pathOutput,
+    // Required by dev server/ HMR
     publicPath: '/'
   },
   module: {
@@ -50,13 +57,18 @@ module.exports = {
   },
   devtool: 'eval',
   devServer: {
+    // Where to serve content from
     contentBase: config.pathOutput,
+    // Hot yes please
     hot: true,
+    // Required by dev server/ HMR
     publicPath: '/',
   },
   plugins: [
+    // Prints more readable module names in the browser console on HMR updates
     new webpack.NamedModulesPlugin(),
     new webpack.HotModuleReplacementPlugin(),
+    // We want to write our own index.html file from a Handlebars template
     new htmlWebpackPlugin({
       template: config.pathSource + config.pathSourceTemplate + config.filenameTemplate,
       filename: config.filenameHTML
